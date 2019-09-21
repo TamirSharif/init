d2a288965bb2ab336c5652af6bb9e3a5b5733647
Tamir Sharif <30700375+TamirSharif@users.noreply.github.com>
2019-09-21 13:03:22 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ npm install -g nodemon
 
 echo -e "\e[42mInstalled:"
 echo -e "\e[0m    ★ Node.js\n    ★ NPM\n    ★ Certbot\n    ★ Nodemon"
+echo "A 'Hello World' index.js file has also been added. Enter a HTTPS Certificate location and download all the required modules"
 
 rm init -r
