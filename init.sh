b1fb9c10d0b364c6d465907ef15134e0339b8ecd
Tamir Sharif <30700375+TamirSharif@users.noreply.github.com>
2019-09-21 12:41:28 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ cp ~/init/index.js ~/index.js
 cd ~/
 npm install -g nodemon
 
-echo -e "\e[42mInstalled:\e[0\n    ★ Node.js\n    ★ NPM\n    ★ Certbot\n    ★ Nodemon"
+echo -e "\e[42mInstalled:"
+echo -e "\e[0m    ★ Node.js\n    ★ NPM\n    ★ Certbot\n    ★ Nodemon"
 
 rm init -r
