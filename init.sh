3d6ca4a8c6cdb9e1148e07c6b33d232fb26c694b
Tamir Sharif <30700375+TamirSharif@users.noreply.github.com>
2019-09-26 18:38:41 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,25 +1,20 @@
-echo -e "\e[42mUpdating Software"
-echo -e "\e[0m"
+echo -e "\e[42mUpdating Software\e[0m"
 apt update -y
 apt upgrade -y
 
-echo -e "\e[42mInstalling Node and NPM"
-echo -e "\e[0m"
+echo -e "\e[42mInstalling Node and NPM\e[0m"
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 apt install nodejs -y
 
-echo -e "\e[42mInstalling Certbot (For adding HTTPS certificates)"
-echo -e "\e[0m"
+echo -e "\e[42mInstalling Certbot (For adding HTTPS certificates)\e[0m"
 apt-get install certbot -y
 certbot certonly --standalone
 
-echo -e "\e[42mInstalling Nodemon"
-echo -e "\e[0m"
+echo -e "\e[42mInstalling Nodemon\e[0m"
 npm install -g nodemon
 
 
-echo -e "\e[42mWhat is the name of your project?"
-echo -e "\e[0m"
+echo -e "\e[42mWhat is the name of your project?\e[0m"
 read projectName
 
 mkdir $projectName
@@ -27,10 +22,9 @@ mkdir $projectName
 cd $projectName
 
 npm init -y
-npm install express http https path fs react react-dom babel-core babel-loader babel-preset-env babel-preset-react --save
+npm install express http https path fs --save
 
-echo -e "\e[42mAdding Hello World index.js file"
-echo -e "\e[0m"
+echo -e "\e[42mAdding Hello World index.js file\e[0m"
 cp ~/init/index.js ~/$projectName/index.js
 cd ~/
 rm init -r
